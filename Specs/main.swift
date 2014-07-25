c7ab1d4e352e8708394fdccf0cef78640b1b3fc0
AlexDenisov <1101.debian@gmail.com>
2014-07-25 19:32:51 +0300

    Specs target added

---
+++ Specs/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Specs
+//
+//  Created by AlexDenisov on 7/25/14.
+//  Copyright (c) 2014 railsware. All rights reserved.
+//
+
+import Foundation
+import Sleipnir
+
+Runner.run()
